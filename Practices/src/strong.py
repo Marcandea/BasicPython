4b3c9f33b8ca6c450917c26e7a8ab1c0b7dd5910
Marcan <marcandea2@gmail.com>
2019-07-28 17:06:16 -0400

    adding test

--- Practices/src/strong.py
+++ Practices/src/strong.py
@@ -1,20 +1,20 @@
 class Strong:
     def strongNumber(self, num1: int):
         string = str(num1)
-        num=0
-        mult=1
-        result=0
+        num = 0
+        mult = 1
+        result = 0
         for i in string:
             entero = int(i)
             for n in range(entero):
-               num=entero-n
-               mult=mult*num
+                num = entero - n
+                mult = mult * num
 
-               #print(num,mult)
+                # print(num,mult)
 
-            result=mult+result
-            mult=1
-        if result==num1:
+            result = mult + result
+            mult = 1
+        if result == num1:
             return "Strong"
         else:
             return "No Strong"
--- Practices/test/test_strong.py
+++ Practices/test/test_strong.py
@@ -1,20 +1,67 @@
 from unittest import TestCase
-from BasicPython.Practices.src.strong import Strong
-class TestStrong (TestCase):
+from src.strong import Strong
+
+
+class TestStrong(TestCase):
     def test_noNtrong(self):
-        #Arranque
+        # Arranque
         st = Strong()
-        #Assert
-        result=st.strongNumber(454)
-        #Assert
-        result_expected="No Strong"
+        # Assert
+        result = st.strongNumber(454)
+        # Assert
+        result_expected = "No Strong"
         self.assertEqual(result_expected, result)
 
     def test_Strong(self):
-        #Arranque
+        # Arranque
         st = Strong()
-        #Assert
-        result=st.strongNumber(145)
-        #Assert
-        result_expected="Strong"
+        # Assert
+        result = st.strongNumber(145)
+        # Assert
+        result_expected = "Strong"
+        self.assertEqual(result_expected, result)
+
+    def test_strong_1_strong(self):
+        # Arranque
+        st = Strong()
+        # Assert
+        result = st.strongNumber(1)
+        # Assert
+        result_expected = "Strong"
+        self.assertEqual(result_expected, result)
+
+    def test_strong_2_strong(self):
+        # Arranque
+        st = Strong()
+        # Assert
+        result = st.strongNumber(1)
+        # Assert
+        result_expected = "Strong"
+        self.assertEqual(result_expected, result)
+
+    def test_strong_123_strong(self):
+        # Arranque
+        st = Strong()
+        # Assert
+        result = st.strongNumber(123)
+        # Assert
+        result_expected = "No Strong"
+        self.assertEqual(result_expected, result)
+
+    def test_strong_negative_number(self):
+        # Arranque
+        st = Strong()
+        # Assert
+        result = st.strongNumber(123)
+        # Assert
+        result_expected = "Negative Number"
+        self.assertEqual(result_expected, result)
+
+    def test_strong_zero_number_strong(self):
+        # Arranque
+        st = Strong()
+        # Assert
+        result = st.strongNumber(0)
+        # Assert
+        result_expected = "No Strong"
         self.assertEqual(result_expected, result)
